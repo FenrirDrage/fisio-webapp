da55724843fab5b6fcc0846f1c08ce31d99ab536
Sérgio Alves <123397767+FenrirDrage@users.noreply.github.com>
2024-12-19 14:43:56 +0000

    correçoes cores

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -3,19 +3,21 @@ import "../css/Sobre.css"; // Estilos para a página Sobre
 
 const Sobre = () => {
   return (
-    <div className="sobre-container">
-      <h1 className="sobre-title">Sobre Nós</h1>
-      <p className="sobre-text">
-        Somos uma clínica de fisioterapia dedicada ao bem-estar e à recuperação
-        física dos nossos pacientes. Com profissionais qualificados e técnicas
-        modernas, oferecemos serviços personalizados para atender às suas
-        necessidades específicas.
-      </p>
-      <p className="sobre-text">
-        A nossa missão é promover saúde e qualidade de vida através de um
-        atendimento humanizado e eficiente.
-      </p>
-    </div>
+    <section className="about">
+      <div className="sobre-container">
+        <h1 className="sobre-title">Sobre Nós</h1>
+        <p className="sobre-text">
+          Somos uma clínica de fisioterapia dedicada ao bem-estar e à
+          recuperação física dos nossos pacientes. Com profissionais
+          qualificados e técnicas modernas, oferecemos serviços personalizados
+          para atender às suas necessidades específicas.
+        </p>
+        <p className="sobre-text">
+          A nossa missão é promover saúde e qualidade de vida através de um
+          atendimento humanizado e eficiente.
+        </p>
+      </div>
+    </section>
   );
 };
 
